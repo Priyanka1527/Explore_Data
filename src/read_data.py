0f510c33ca071a5dc108bb9833fa43417b479caf
Priyanka Saha <priyanka@Priyankas-MacBook-Air.local>
2018-09-05 21:36:14 -0500

    added dataframe join notebook

--- src/read_data.py
+++ src/read_data.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
-#read the dataset into pandas dataframe
-data = pd.read_csv("../data/DS2.csv")
-print (data.head())
+#Below are some of the usefult command for PAndas
+#print (data.head(4))
+#print (data.values) //shows values of each row in array
+#print (data.describe) #quick statistic summary
+#print (data['Primary Type']) //displays the speciifc columns
+#print (data.iloc[3:6,0:3]) #displays Rows 3-5 and column 0-2
+#print(data.iloc[3]) //displays 3rd Row
+#print (data.iloc[0,3]) //displays 0th Row, 3rd column
+#data2 = data.cumsum()
+#print (data2.plot())
+
+crime_list = pd.read_csv("../data/DS2.csv")
+crime_list = crime_list.set_index('Case Number')
+assault_list = pd.read_csv("../data/DS1.csv")
+crime_assault = assault_list.join(crime_list, on=['Case Number'])
+print (crime_assault)
